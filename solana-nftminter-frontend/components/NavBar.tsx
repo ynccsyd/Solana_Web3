1c096983c90d6b7b719439c0f14526ce6e5056ea
ynccsyd <muhibbi_10_@hotmaile.com>
2022-12-02 19:54:44 +0300

    Added Navbar&css

---
+++ solana-nftminter-frontend/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { HStack, Spacer } from "@chakra-ui/react"
+import { WalletMultiButton } from "@solana/wallet-adapter-react-ui"
+import { FC } from "react"
+import styles from "../styles/Home.module.css"
+
+const NavBar: FC = () => {
+  return (
+    <HStack width="full" padding={4}>
+      <Spacer />
+      <WalletMultiButton className={styles["wallet-adapter-button-trigger"]} />
+    </HStack>
+  )
+}
+
+export default NavBar
